f61c3b777092253f50af8a7b7fe6aa2ce334322d
Bit Borealis <theotheroracle@disroot.org>
2025-03-23 21:20:02 -0400

    change udev file prefix to 60

--- pkgs/by-name/sy/sysdvr-udev/package.nix
+++ pkgs/by-name/sy/sysdvr-udev/package.nix
@@ -16,7 +16,7 @@ stdenv.mkDerivation rec {
   };
 
   installPhase = ''
-    install -Dpm644 $src/Client/Platform/Linux/sysdvr.rules $out/lib/udev/rules.d/50-wooting.rules
+    install -Dpm644 $src/Client/Platform/Linux/sysdvr.rules $out/lib/udev/rules.d/60-sysdvr.rules
   '';
 
   meta = with lib; {
